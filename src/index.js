5f6aba9ad00243c0772e8cdcfc17eaf13b761c57
Viktor Poklad <3d.poklad@gmail.com>
2021-08-08 20:23:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,5 +35,5 @@ function onStartClick() {
 
 
 const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
